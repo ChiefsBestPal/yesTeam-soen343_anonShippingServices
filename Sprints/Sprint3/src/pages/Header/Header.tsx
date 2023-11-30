7f5f2e6b83e944218b3efa9a93f97252b018368f
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2023-11-30 16:36:51 -0500

    Added logged in user responsive display + fixed some header/footer flex bugs

--- Sprints/Sprint3/src/pages/Header/Header.tsx
+++ Sprints/Sprint3/src/pages/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate, Navigate, NavLink} from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import './Header.css'; // Make sure the path is correct
 import companyLogo from './Company logo.png';
 
@@ -20,9 +21,16 @@ const Header: React.FC = () => {
             <li className="nav-item"><a href="/delivery">Ship/Deliver</a></li>
             <li className="nav-item"><a href="/tracking">Track</a></li>
             <li className="nav-item"><a href="/review">Reviews</a></li>
-            <li className="nav-item"><a href="">Login</a></li>
+            {/*<li className="nav-item"><a href="">Login</a></li>*/}
+
           </ul>
+
+          <div className="user-profile">
+            <div className="user-icon-wrapper"><FontAwesomeIcon icon={faUserSecret} className="user-icon" /></div>
+            <span className="username">{"Logged in: Admin"}</span>
+          </div>
         </nav>
+
       </div>
       <div className="grad-bar"></div>
     </header>
