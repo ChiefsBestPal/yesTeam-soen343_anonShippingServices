4104b724dc7b69ec69ac0d8b54490a5d281c1d58
AA789-ai <sonubhaii883@gmail.com>
2023-11-30 14:36:04 -0500

    Review fixed

--- Sprints/Sprint3/src/pages/Review/Review.tsx
+++ Sprints/Sprint3/src/pages/Review/Review.tsx
@@ -78,82 +78,82 @@ const Reviews: React.FC = () => {
     };
 
     return (
-        <div className='Review'>
-            <div className="background-top"></div>
-            <div className="background-bottom"></div>
+        <div className='Review-o'>
+            <div className="background-top-o"></div>
+            <div className="background-bottom-o"></div>
             <div className='title-main'>Submit Your Review</div>
-            <div className='Rectangle'>
-                <div className='contain-1'>
-                    <div className='name-anon c1'>
-                        <div className='name'>
+            <div className='Rectangle-o'>
+                <div className='contain-1-o'>
+                    <div className='name-anon-o c1-o'>
+                        <div className='name-o'>
                             <div>Name:</div>
-                            <input className='input' type="text" name="name" value={reviewData.name} onChange={handleInputChange} disabled={reviewData.isAnonymous} />
+                            <input className='input-o' type="text" name="name" value={reviewData.name} onChange={handleInputChange} disabled={reviewData.isAnonymous} />
                         </div>
-                        <div className='name'>
+                        <div className='name-o'>
                             Anonymous:
-                            <input className='input-check' type="checkbox" name="isAnonymous" checked={reviewData.isAnonymous} onChange={handleInputChange} />
+                            <input className='input-check-o' type="checkbox" name="isAnonymous" checked={reviewData.isAnonymous} onChange={handleInputChange} />
                         </div>
                     </div>
 
-                    <div className='rating-review c1'>
-                        <div className='name'>
+                    <div className='rating-review-o c1-o'>
+                        <div className='name-o'>
                             Rating:
                         </div>
-                        <div className='star'>
+                        <div className='star-o'>
                             {renderStars()}
                         </div>
                     </div>
 
-                    <div className='title c1'>
-                        <div className='name'>
+                    <div className='title-o c1-o'>
+                        <div className='name-o'>
                             Review Title:
                         </div>
-                        <input className='input review-title' type="text" name="reviewTitle" value={reviewData.reviewTitle} onChange={handleInputChange} />
+                        <input className='input review-title-o' type="text" name="reviewTitle" value={reviewData.reviewTitle} onChange={handleInputChange} />
                     </div>
                 </div>
 
 
-                <div className='contain-2'>
-                    <div className='title' >
-                        <div className='name'>
+                <div className='contain-2-o'>
+                    <div className='title-o' >
+                        <div className='name-o'>
                             Order Number (Auto generated):
                         </div>
-                        <input className='input' type="text" name="reviewTitle" value={reviewData.orderNumber} onChange={handleInputChange} />
+                        <input className='input-o' type="text" name="reviewTitle" value={reviewData.orderNumber} onChange={handleInputChange} />
                     </div>
-                    <div className='title'>
-                        <div className='name'>
+                    <div className='title-o'>
+                        <div className='name-o'>
                             Did your order arrive on time?   
                         </div>
-                        <input className='input' type="radio" name="question1" value="Yes" checked={reviewData.answers.question1 === 'Yes'} onChange={handleInputChange} /> Yes
-                        <input className='input' type="radio" name="question1" value="No" checked={reviewData.answers.question1 === 'No'} onChange={handleInputChange} /> No
+                        <input className='input-o' type="radio" name="question1" value="Yes" checked={reviewData.answers.question1 === 'Yes'} onChange={handleInputChange} /> Yes
+                        <input className='input-o' type="radio" name="question1" value="No" checked={reviewData.answers.question1 === 'No'} onChange={handleInputChange} /> No
                     </div>
-                    <div className='title'>
-                        <div className='name'>
+                    <div className='title-o'>
+                        <div className='name-o'>
                             Was your order complete and undamaged?
                         </div>
-                        <input className='input' type="radio" name="question2" value="Yes" checked={reviewData.answers.question2 === 'Yes'} onChange={handleInputChange} /> Yes
-                        <input className='input' type="radio" name="question2" value="No" checked={reviewData.answers.question2 === 'No'} onChange={handleInputChange} /> No
+                        <input className='input-o' type="radio" name="question2" value="Yes" checked={reviewData.answers.question2 === 'Yes'} onChange={handleInputChange} /> Yes
+                        <input className='input-o' type="radio" name="question2" value="No" checked={reviewData.answers.question2 === 'No'} onChange={handleInputChange} /> No
                     </div >
-                    <div className='title'>
-                        <div className='name'>
+                    <div className='title-o'>
+                        <div className='name-o'>
                             Were you satisfied with the packaging?
                         </div>
-                        <input className='input' type="radio" name="question3" value="Yes" checked={reviewData.answers.question3 === 'Yes'} onChange={handleInputChange} /> Yes
-                        <input className='input' type="radio" name="question3" value="No" checked={reviewData.answers.question3 === 'No'} onChange={handleInputChange} /> No
+                        <input className='input-o' type="radio" name="question3" value="Yes" checked={reviewData.answers.question3 === 'Yes'} onChange={handleInputChange} /> Yes
+                        <input className='input-o' type="radio" name="question3" value="No" checked={reviewData.answers.question3 === 'No'} onChange={handleInputChange} /> No
                     </div>
                 </div>
-                <div className='contain-3'>
+                <div className='contain-3-o'>
                         <div>Review:</div>
                         <textarea 
-                        className='input comment-box' 
+                        className='input-o comment-box-o' 
                         name="review" 
                         value={reviewData.review} 
                         onChange={handleReviewChange} 
                         />
                 </div>
             </div>
-            <div className='buttons'>
-                <button type="button" className='tracking-button' onClick={handleFileButtonClick}>
+            <div className='buttons-o'>
+                <button type="button" className='tracking-button-o' onClick={handleFileButtonClick}>
                         Attach File
                 </button>
                 <input 
@@ -163,7 +163,7 @@ const Reviews: React.FC = () => {
                     ref={fileInputRef} 
                     style={{ display: 'none' }} 
                 />
-                <button className='tracking-button' type="submit">Submit Review</button>
+                <button className='tracking-button-o' type="submit">Submit Review</button>
 
                 
             </div>
